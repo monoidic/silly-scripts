7b92a696bb08793205ca974097419282796f75fd
monoidic <36573837+monoidic@users.noreply.github.com>
2018-12-14 02:20:48 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, re
+import sys, urllib.parse
 
 if len(sys.argv) != 2:
   print("Invalid amount of arguments passed")
@@ -9,28 +9,30 @@ text = sys.argv[1]
 
 class TranslateClass(object):
   def __getitem__(self, ordinal):
-    hexout = ""
     hexchar = bytes(chr(ordinal), "UTF-8").hex()
     special_chars = (
-      "!","*","'","(",")",";",":","@","&",
-      "=","+","$",",","/","?","#","[","]"
-    ) # "."	rfc3986
+      ":", "/", "?", "#", "[", "]", "@", "!", "$",
+      "&", "'", "(", ")", "*", "+", ",", ";", "="
+    ) #	rfc3986
     if chr(ordinal) in special_chars:
       return(chr(ordinal))
-    elif len(hexchar) > 0:
-      for i in range(len(hexchar)):
-        if i % 2 == 0:
-          hexout += "%"
-        hexout += hexchar[i]
+    hexout = ""
+    for i in range(len(hexchar)):
+      if i % 2 == 0:
+        hexout += "%"
+      hexout += hexchar[i]
     return(hexout)
 
-translateobject = TranslateClass()
-prefixmatch = re.match(".*://", text)
+tr_obj = TranslateClass()
+parsed = urllib.parse.urlparse(text)
+if parsed.scheme == "":
+  parsed = urllib.parse.urlparse("http://" + text)
 
-if prefixmatch == None:
-  prefix = ""
-else:
-  prefix = prefixmatch.group()
+portstuff = ":" + str(parsed.port) if parsed.port != None else ""
+querystuff = "?" + parsed.query.translate(tr_obj) if parsed.query != "" else ""
+fragmentstuff = "#" + parsed.fragment.translate(tr_obj) if parsed.fragment != "" else ""
 
-translated = prefix + text[len(prefix):].translate(translateobject)
+
+translated = parsed.scheme + "://" + parsed.hostname.translate(tr_obj) + portstuff
+translated = translated + parsed.path.translate(tr_obj) + querystuff + fragmentstuff
 print(translated)
