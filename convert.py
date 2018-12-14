b8512136aacde3ca187cd57a9c429bd27edd6490
monoidic <36573837+monoidic@users.noreply.github.com>
2018-12-14 08:53:37 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,7 +5,10 @@ import sys, urllib.parse
 if len(sys.argv) != 2:
   print("Invalid amount of arguments passed")
   exit(1)
-text = sys.argv[1]
+
+parsed = urllib.parse.urlparse(sys.argv[1])
+if parsed.scheme == "":
+  parsed = urllib.parse.urlparse("//" + sys.argv[1])
 
 class TranslateClass(object):
   def __getitem__(self, ordinal):
@@ -24,12 +27,9 @@ class TranslateClass(object):
     return hexout
 
 tr_obj = TranslateClass()
-parsed = urllib.parse.urlparse(text)
-if parsed.scheme == "":
-  parsed = urllib.parse.urlparse("//" + text)
 
 schemestuff = parsed.scheme + "://" if parsed.scheme else ""
-portstuff = ":" + str(parsed.port) if parsed.port != None else ""
+portstuff = ":" + str(parsed.port) if parsed.port else ""
 querystuff = "?" + parsed.query.translate(tr_obj) if parsed.query else ""
 fragmentstuff = "#" + parsed.fragment.translate(tr_obj) if parsed.fragment  else ""
 
