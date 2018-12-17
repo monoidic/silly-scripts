285afbf5c0e63beb04adbbb900153c48cfbc3f4d
monoidic <36573837+monoidic@users.noreply.github.com>
2018-12-17 23:44:26 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -6,10 +6,6 @@ if len(sys.argv) != 2:
   print("Invalid amount of arguments passed")
   exit(1)
 
-parsed = urllib.parse.urlparse(sys.argv[1])
-if parsed.scheme == "":
-  parsed = urllib.parse.urlparse("//" + sys.argv[1])
-
 class TranslateClass(object):
   def __getitem__(self, ordinal):
     hexchar = bytes(chr(ordinal), "UTF-8").hex()
@@ -28,12 +24,15 @@ class TranslateClass(object):
 
 tr_obj = TranslateClass()
 
-schemestuff = parsed.scheme + "://" if parsed.scheme else ""
+parsed = urllib.parse.urlparse(sys.argv[1])
+if parsed.netloc == "":
+  parsed = urllib.parse.urlparse("noscheme://" + sys.argv[1])
+
+schemestuff = parsed.scheme + "://" if (parsed.scheme and parsed.scheme != "noscheme") else ""
 portstuff = ":" + str(parsed.port) if parsed.port else ""
 querystuff = "?" + parsed.query.translate(tr_obj) if parsed.query else ""
-fragmentstuff = "#" + parsed.fragment.translate(tr_obj) if parsed.fragment  else ""
-
+fragmentstuff = "#" + parsed.fragment.translate(tr_obj) if parsed.fragment else ""
 
 translated = schemestuff + parsed.hostname.translate(tr_obj) + portstuff
-translated = translated + parsed.path.translate(tr_obj) + querystuff + fragmentstuff
+translated += parsed.path.translate(tr_obj) + querystuff + fragmentstuff
 print(translated)
