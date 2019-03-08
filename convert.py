7c848c98a5bdd96f71641fa45bf477cbff68fd0a
monoidic <monoidic@users.noreply.github.com>
2019-03-08 07:59:27 +0200

    Rework URL building in convert.py

--- convert.py
+++ convert.py
@@ -3,36 +3,39 @@
 import sys, urllib.parse
 
 if len(sys.argv) != 2:
-  print("Invalid amount of arguments passed")
+  print('Invalid amount of arguments passed')
   exit(1)
 
 class TranslateClass(object):
   def __getitem__(self, ordinal):
     special_chars = (
-      ":", "/", "?", "#", "[", "]", "@", "!", "$",
-      "&", "'", "(", ")", "*", "+", ",", ";", "="
+      ':', '/', '?', '#', '[', ']', '@', '!', '$',
+      '&', "'", '(', ')', '*', '+', ',', ';', '='
     ) #	rfc3986
     if chr(ordinal) in special_chars:
-      return chr(ordinal)
-    hexchar = bytes(chr(ordinal), "UTF-8").hex()
-    hexout = ""
+      return ordinal
+    hexchar = bytes(chr(ordinal), 'UTF-8').hex()
+    hexout = ''
     for i in range(len(hexchar)):
       if i % 2 == 0:
-        hexout += "%"
+        hexout += '%'
       hexout += hexchar[i]
     return hexout
 
 tr_obj = TranslateClass()
 
 parsed = urllib.parse.urlparse(sys.argv[1])
-if parsed.netloc == "":
-  parsed = urllib.parse.urlparse("//" + sys.argv[1])
+if parsed.netloc == '':
+  parsed = urllib.parse.urlparse('//' + sys.argv[1])
 
-schemestuff = parsed.scheme + "://" if parsed.scheme else ""
-portstuff = ":" + str(parsed.port) if parsed.port else ""
-querystuff = "?" + parsed.query.translate(tr_obj) if parsed.query else ""
-fragmentstuff = "#" + parsed.fragment.translate(tr_obj) if parsed.fragment else ""
+newurl = (
+  parsed.scheme if parsed.scheme else '',
+  parsed.hostname.translate(tr_obj) +
+    str(':' + str(parsed.port) if parsed.port else ''),
+  parsed.path.translate(tr_obj),
+  '',
+  parsed.query.translate(tr_obj) if parsed.query else '',
+  parsed.fragment.translate(tr_obj) if parsed.fragment else ''
+)
 
-translated = schemestuff + parsed.hostname.translate(tr_obj) + portstuff
-translated += parsed.path.translate(tr_obj) + querystuff + fragmentstuff
-print(translated)
+print(urllib.parse.urlunparse(newurl))
